5e71d70548d6ff0c2bfbb5a70382df81418ca066
kalsmith <nsuversivo@yahoo.es>
2026-03-11 16:48:33 -0300

    update de nuevo flujo

--- app/Http/Controllers/Patient/PatientOrderController.php
+++ app/Http/Controllers/Patient/PatientOrderController.php
@@ -130,7 +130,7 @@ class PatientOrderController extends Controller
     // Cargamos relaciones para el comprobante
     $order->load(['patient.user', 'paymentTransaction']);
 
-    return view('patient.payment-success', compact('order'));
+return view('payments.payment-success', compact('order'));
 }
 
 }
