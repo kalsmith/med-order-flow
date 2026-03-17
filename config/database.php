548a2535e6222a531e0b8fadbb59d7f1ac1a43a3
kalsmith <nsuversivo@yahoo.es>
2026-03-17 09:20:09 -0300

    update hora

--- config/database.php
+++ config/database.php
@@ -54,7 +54,6 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'timezone' => 'America/Santiago',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
